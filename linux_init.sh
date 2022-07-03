b1a0829ef18affc31c8578516b6e0ec92486cce7
agnatyuk <agnatyuk@croc.ru>
2022-07-04 01:16:01 +0300

    add gitconf and linux init

---
+++ linux_init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# required packages
+sudo apt install -y git tig curl wget vim zsh make openvpn htop
+
+# python dev packages
+sudo apt install -y python3-pip build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
