2ee1858666bc94872c69176d645713b9d606f1c1
Alexander Gnatyuk <agnatyuk@croc.ru>
2022-07-05 02:14:17 +0300

    awesomewm installation

--- linux_init.sh
+++ linux_init.sh
@@ -5,3 +5,11 @@ sudo apt install -y git tig curl wget vim zsh make openvpn htop
 
 # python dev packages
 sudo apt install -y python3-pip build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev
+
+
+# setup awesome wm 18.04ubuntu
+sudo apt-get install -y gnome gnome-flashback gnome-icon-theme gnome-themes-extra awesome
+
+git clone https://github.com/SirJson/awesomewm-gnome.git
+# make install in directory
+
